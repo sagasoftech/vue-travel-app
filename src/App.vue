f9e69abec47124893d6f7d7c638ad5bbe17b2f17
Sagar Dighe <sagasoftech@gmail.com>
2024-02-28 23:49:08 -0600

    Code cleanup

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,6 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-    <img alt="Vue logo" src="@/assets/logo.svg" width="125" height="125" />
-    <br>
     <div>
         <RouterLink to="/">Home</RouterLink> | 
         <RouterLink to="/about">About</RouterLink>
