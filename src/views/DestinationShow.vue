efc4e0981de77d173f1b7301bfd28905e649a4b0
Sagar Dighe <sagasoftech@gmail.com>
2024-03-01 00:19:43 -0600

    Revert API changes and move back to fetch data from data.json file

--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,5 +1,5 @@
 <template>
-    <section v-if="destination" class="destination">
+    <section class="destination">
         <h1>{{ destination.name }}</h1>
         <div class="destination-details">
             <img :src="`/images/${ destination.image }`" :alt="destination.name">
@@ -10,31 +10,14 @@
 
 <script>
 import sourceData from '../data.json'
-
 export default{
-
-  data(){
-      return{
-          destination : null
-      }
-  },
   computed:{
     destinationId(){
         return parseInt(this.$route.params.id)
+    },
+    destination(){
+        return sourceData.destinations.find(destination => destination.id === this.destinationId)
     }
-  },
-  methods:{
-    async initData(){
-        const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}.json`)
-        this.destination = await response.json()
-    }
-  },
-  async created(){
-
-    //First time page load
-    this.initData()
-
   }
-
 }
 </script>
