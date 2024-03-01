f910970124e3eec2cabe1d05a661a48105c973c8
sagasoftech <63987224+sagasoftech@users.noreply.github.com>
2024-02-29 22:09:22 -0600

    Loading home page data using json file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>
-    <h1>This is an home page</h1>
+  <div class="home">
+    <h1>All Destinations</h1>
+    <div class="destinations">
+      <router-link 
+      v-for="destination in destinations"
+      :key="destination.id"
+      :to="destination.slug"
+      >
+        <h2>{{ destination.name }}</h2>
+        <img :src="`/images/${ destination.image }`" :alt="destination.name">
+      </router-link>
+
+    </div>
   </div>
 </template>
+
+
+<script>
+import sourceData from '../data.json'
+
+export default{
+  data(){
+    return{
+      destinations: sourceData.destinations
+    }
+  }
+}
+</script>
