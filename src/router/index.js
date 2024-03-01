9d0713ce8d7bf3d10adae5f4dd0500ce634d3b6d
sagasoftech <63987224+sagasoftech@users.noreply.github.com>
2024-02-29 22:16:05 -0600

    Code formatting

--- src/router/index.js
+++ src/router/index.js
@@ -3,36 +3,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: ()=> import('@/views/AboutView.vue')
-  },
-  {
-    path: '/brazil',
-    name: 'brazil',
-    component: ()=> import('@/views/Brazil.vue')
-  },
-  {
-    path: '/hawaii',
-    name: 'hawaii',
-    component: ()=> import('@/views/Hawaii.vue')
-  },
-  {
-    path: '/jamaica',
-    name: 'jamaica',
-    component: ()=> import('@/views/Jamaica.vue')
-  },
-  {
-    path: '/panama',
-    name: 'panama',
-    component: ()=> import('@/views/Panama.vue')
-  }
+  {path: '/', name: 'home', component: HomeView},
+
+  {path: '/about', name: 'about', component: ()=> import('@/views/AboutView.vue')},
+
+  {path: '/brazil', name: 'brazil', component: ()=> import('@/views/Brazil.vue')},
+
+  {path: '/hawaii', name: 'hawaii', component: ()=> import('@/views/Hawaii.vue')},
+
+  {path: '/jamaica', name: 'jamaica', component: ()=> import('@/views/Jamaica.vue')},
+
+  {path: '/panama', name: 'panama', component: ()=> import('@/views/Panama.vue')}
+
+  
 ]
 
 const router = createRouter({
